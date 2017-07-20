ce321593c5c4ce8f6a554d628a92bdd6fdd04c3c
Shweta <Shweta>
2017-07-20 22:17:48 +0530

    initial commit

---
+++ app/src/main/java/com/water/FormActivity.java
@@ -0,0 +1,27 @@
+package com.water;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FormActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.btn_go).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_go:
+                startActivity(new Intent(this, DrinkActivity.class));
+                break;
+        }
+    }
+}
